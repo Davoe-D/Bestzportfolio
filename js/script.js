0ba7e29042888af532383b622b4058efe138c8a4
I_am_BB <38059070+solomonugo@users.noreply.github.com>
2021-10-26 20:17:04 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+function toggle(){
+  document.getElementById("nav-container").id = "#dark_nav"
+  document.querySelector("header").id = ".dark_header"
+  document.querySelector("body").id = ".dark_body"
+  document.querySelector(".content").id = ".dark_content"
+  document.querySelector(".skills").id = ".dark_skills"
+  
+}
+
+
+
